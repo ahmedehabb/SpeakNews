04c19388e808b552d4a18ec23c5637aeadf74cb9
ahmedehabb <ahmedehabb17@gmail.com>
2023-06-02 03:42:56 +0200

    added summarizers module

---
+++ Summarizers/bert based summarizers/src/models/model_builder.py
@@ -0,0 +1,279 @@
+import copy
+
+import torch
+import torch.nn as nn
+from pytorch_transformers import BertModel, BertConfig
+from torch.nn.init import xavier_uniform_
+
+from models.decoder import TransformerDecoder
+from models.encoder import Classifier, ExtTransformerEncoder
+from models.optimizers import Optimizer
+
+def build_optim(args, model, checkpoint):
+    """ Build optimizer """
+
+    if checkpoint is not None:
+        optim = checkpoint['optim'][0]
+        saved_optimizer_state_dict = optim.optimizer.state_dict()
+        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
+        if args.visible_gpus != '-1':
+            for state in optim.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.cuda()
+
+        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
+            raise RuntimeError(
+                "Error: loaded Adam optimizer from existing model" +
+                " but optimizer state is empty")
+
+    else:
+        optim = Optimizer(
+            args.optim, args.lr, args.max_grad_norm,
+            beta1=args.beta1, beta2=args.beta2,
+            decay_method='noam',
+            warmup_steps=args.warmup_steps)
+
+    optim.set_parameters(list(model.named_parameters()))
+
+
+    return optim
+
+def build_optim_bert(args, model, checkpoint):
+    """ Build optimizer """
+
+    if checkpoint is not None:
+        optim = checkpoint['optims'][0]
+        saved_optimizer_state_dict = optim.optimizer.state_dict()
+        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
+        if args.visible_gpus != '-1':
+            for state in optim.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.cuda()
+
+        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
+            raise RuntimeError(
+                "Error: loaded Adam optimizer from existing model" +
+                " but optimizer state is empty")
+
+    else:
+        optim = Optimizer(
+            args.optim, args.lr_bert, args.max_grad_norm,
+            beta1=args.beta1, beta2=args.beta2,
+            decay_method='noam',
+            warmup_steps=args.warmup_steps_bert)
+
+    params = [(n, p) for n, p in list(model.named_parameters()) if n.startswith('bert.model')]
+    optim.set_parameters(params)
+
+
+    return optim
+
+def build_optim_dec(args, model, checkpoint):
+    """ Build optimizer """
+
+    if checkpoint is not None:
+        optim = checkpoint['optims'][1]
+        saved_optimizer_state_dict = optim.optimizer.state_dict()
+        optim.optimizer.load_state_dict(saved_optimizer_state_dict)
+        if args.visible_gpus != '-1':
+            for state in optim.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.cuda()
+
+        if (optim.method == 'adam') and (len(optim.optimizer.state) < 1):
+            raise RuntimeError(
+                "Error: loaded Adam optimizer from existing model" +
+                " but optimizer state is empty")
+
+    else:
+        optim = Optimizer(
+            args.optim, args.lr_dec, args.max_grad_norm,
+            beta1=args.beta1, beta2=args.beta2,
+            decay_method='noam',
+            warmup_steps=args.warmup_steps_dec)
+
+    params = [(n, p) for n, p in list(model.named_parameters()) if not n.startswith('bert.model')]
+    optim.set_parameters(params)
+
+
+    return optim
+
+
+def get_generator(vocab_size, dec_hidden_size, device):
+    gen_func = nn.LogSoftmax(dim=-1)
+    generator = nn.Sequential(
+        nn.Linear(dec_hidden_size, vocab_size),
+        gen_func
+    )
+    generator.to(device)
+
+    return generator
+
+class Bert(nn.Module):
+    def __init__(self, large, temp_dir, finetune=False):
+        super(Bert, self).__init__()
+        if(large):
+            self.model = BertModel.from_pretrained('bert-large-uncased', cache_dir=temp_dir)
+        else:
+            self.model = BertModel.from_pretrained('bert-base-uncased', cache_dir=temp_dir)
+
+        self.finetune = finetune
+
+    def forward(self, x, segs, mask):
+        if(self.finetune):
+            # if funetune flag is true, we use model in train mode and tune parameters
+            top_vec, _ = self.model(x, segs, attention_mask=mask)
+        else:
+            # else, we set it to eval mode, so we dont change any parameter.
+            self.eval()
+            with torch.no_grad():
+                top_vec, _ = self.model(x, segs, attention_mask=mask)
+        return top_vec
+
+
+class ExtSummarizer(nn.Module):
+    def __init__(self, args, device, checkpoint):
+        super(ExtSummarizer, self).__init__()
+        self.args = args
+        self.device = device
+        # make an instance of the BERT model used for encoding the input document.
+        # It takes arguments such as large (whether to use the large BERT model), 
+        # temp_dir (directory to store temporary files), 
+        # and finetune_bert (whether to fine-tune the BERT model during training).
+        self.bert = Bert(args.large, args.temp_dir, args.finetune_bert)
+
+        # transformer-based encoding to the sentence representations.
+        self.ext_layer = ExtTransformerEncoder(self.bert.model.config.hidden_size, args.ext_ff_size, args.ext_heads,
+                                               args.ext_dropout, args.ext_layers)
+        if (args.encoder == 'baseline'):
+            bert_config = BertConfig(self.bert.model.config.vocab_size, hidden_size=args.ext_hidden_size,
+                                     num_hidden_layers=args.ext_layers, num_attention_heads=args.ext_heads, intermediate_size=args.ext_ff_size)
+            self.bert.model = BertModel(bert_config)
+            self.ext_layer = Classifier(self.bert.model.config.hidden_size)
+
+        # If args.max_pos is greater than 512, the positional embeddings of the BERT model are extended to accommodate the longer sequences.
+        # This is achieved by creating a new embedding layer and copying the original embeddings up to position 512 and repeating the last embedding for positions beyond 512.
+        if(args.max_pos>512):
+            my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
+            my_pos_embeddings.weight.data[:512] = self.bert.model.embeddings.position_embeddings.weight.data
+            my_pos_embeddings.weight.data[512:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-512,1)
+            self.bert.model.embeddings.position_embeddings = my_pos_embeddings
+
+
+        if checkpoint is not None:
+            self.load_state_dict(checkpoint['model'], strict=True)
+        else:
+            if args.param_init != 0.0:
+                for p in self.ext_layer.parameters():
+                    p.data.uniform_(-args.param_init, args.param_init)
+            if args.param_init_glorot:
+                for p in self.ext_layer.parameters():
+                    if p.dim() > 1:
+                        xavier_uniform_(p)
+
+        self.to(device)
+
+    def forward(self, src, segs, clss, mask_src, mask_cls):
+        # applying the pretrained encoder model (BERT) to the input source document. 
+        # It takes the src input, which represents the tokenized source document, 
+        # segs which indicates the segment or sentence boundaries, 
+        # and mask_src which is a binary mask indicating the valid tokens. 
+        # The output top_vec represents the encoded representations of the source document.
+        top_vec = self.bert(src, segs, mask_src)
+
+        # then we extract the sentence-level representations from the encoded document representations (top_vec).
+        # The clss tensor contains the indices of the selected sentences in the source document. 
+        # It uses advanced indexing to select the corresponding sentence representations from top_vec using the indices provided by clss.
+        sents_vec = top_vec[torch.arange(top_vec.size(0)).unsqueeze(1), clss]
+
+        # apply a mask to the sentence representations (sents_vec). 
+        # The mask_cls tensor indicates the valid sentences in the source document by assigning 1 to valid sentences and 0 to padding sentences.
+        sents_vec = sents_vec * mask_cls[:, :, None].float()
+
+        # This line computes the sentence scores using the ext_layer module.
+        # The sents_vec input contains the masked sentence representations, and the mask_cls input is used to mask out the padded sentences.
+        # The ext_layer module applies a transformation to the sentence representations and produces a score for each sentence.
+        # The squeeze(-1) operation removes the extra dimension, resulting in a 1-dimensional tensor of sentence scores.
+        sent_scores = self.ext_layer(sents_vec, mask_cls).squeeze(-1)
+        return sent_scores, mask_cls
+
+
+class AbsSummarizer(nn.Module):
+    def __init__(self, args, device, checkpoint=None, bert_from_extractive=None):
+        super(AbsSummarizer, self).__init__()
+        self.args = args
+        self.device = device
+        self.bert = Bert(args.large, args.temp_dir, args.finetune_bert)
+
+        if bert_from_extractive is not None:
+            # loads the state dictionary into the BERT model (self.bert.model) that is currently being used.
+            self.bert.model.load_state_dict(
+                #  modifying keys by removing the initial 11 characters from each key. 
+                # This is done to match the key names in the current BERT model's state dictionary.
+                dict([(n[11:], p) for n, p in bert_from_extractive.items() if n.startswith('bert.model')]), strict=True)
+
+        if (args.encoder == 'baseline'):
+            # if baseline, dont load the finetuned extractive model, but use the normal bert encoder 
+            bert_config = BertConfig(self.bert.model.config.vocab_size, hidden_size=args.enc_hidden_size,
+                                     num_hidden_layers=args.enc_layers, num_attention_heads=8,
+                                     intermediate_size=args.enc_ff_size,
+                                     hidden_dropout_prob=args.enc_dropout,
+                                     attention_probs_dropout_prob=args.enc_dropout)
+            self.bert.model = BertModel(bert_config)
+
+        if(args.max_pos>512):
+            my_pos_embeddings = nn.Embedding(args.max_pos, self.bert.model.config.hidden_size)
+            my_pos_embeddings.weight.data[:512] = self.bert.model.embeddings.position_embeddings.weight.data
+            # If the maximum position exceeds 512, it assigns the last position embedding from the original BERT model's 
+            # position embeddings (self.bert.model.embeddings.position_embeddings.weight.data[-1]) to all the remaining positions 
+            # in the new position embeddings layer. This is achieved by repeating the last position embedding args.max_pos-512 times.
+            my_pos_embeddings.weight.data[512:] = self.bert.model.embeddings.position_embeddings.weight.data[-1][None,:].repeat(args.max_pos-512,1)
+            self.bert.model.embeddings.position_embeddings = my_pos_embeddings
+        self.vocab_size = self.bert.model.config.vocab_size
+        tgt_embeddings = nn.Embedding(self.vocab_size, self.bert.model.config.hidden_size, padding_idx=0)
+        if (self.args.share_emb):
+            # By sharing the embeddings, both the BERT model and the target embeddings will use the same underlying word embeddings weights.
+            # This can be beneficial when training a model with limited resources or when fine-tuning a BERT-based model for a specific task where sharing the embeddings can help in leveraging the pre-trained representations effectively.
+            tgt_embeddings.weight = copy.deepcopy(self.bert.model.embeddings.word_embeddings.weight)
+
+        self.decoder = TransformerDecoder(
+            self.args.dec_layers,
+            self.args.dec_hidden_size, heads=self.args.dec_heads,
+            d_ff=self.args.dec_ff_size, dropout=self.args.dec_dropout, embeddings=tgt_embeddings)
+
+        self.generator = get_generator(self.vocab_size, self.args.dec_hidden_size, device)
+        self.generator[0].weight = self.decoder.embeddings.weight
+
+
+        if checkpoint is not None:
+            self.load_state_dict(checkpoint['model'], strict=True)
+        else:
+            for module in self.decoder.modules():
+                if isinstance(module, (nn.Linear, nn.Embedding)):
+                    module.weight.data.normal_(mean=0.0, std=0.02)
+                elif isinstance(module, nn.LayerNorm):
+                    module.bias.data.zero_()
+                    module.weight.data.fill_(1.0)
+                if isinstance(module, nn.Linear) and module.bias is not None:
+                    module.bias.data.zero_()
+            for p in self.generator.parameters():
+                if p.dim() > 1:
+                    xavier_uniform_(p)
+                else:
+                    p.data.zero_()
+            if(args.use_bert_emb):
+                tgt_embeddings = nn.Embedding(self.vocab_size, self.bert.model.config.hidden_size, padding_idx=0)
+                tgt_embeddings.weight = copy.deepcopy(self.bert.model.embeddings.word_embeddings.weight)
+                self.decoder.embeddings = tgt_embeddings
+                self.generator[0].weight = self.decoder.embeddings.weight
+
+        self.to(device)
+
+    def forward(self, src, tgt, segs, clss, mask_src, mask_tgt, mask_cls):
+        top_vec = self.bert(src, segs, mask_src)
+        dec_state = self.decoder.init_decoder_state(src, top_vec)
+        decoder_outputs, state = self.decoder(tgt[:, :-1], top_vec, dec_state)
+        return decoder_outputs, None
---
+++ Summarizers/bert based summarizers/src/train.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+"""
+    Main training workflow
+"""
+from __future__ import division
+
+import argparse
+import os
+from others.logging import init_logger
+from train_abstractive import build_abstractive, summarize_by_abstractive, validate_abs, train_abs, baseline, test_abs, test_text_abs
+from train_extractive import summarize_by_extractive, train_ext, validate_ext, test_ext, test_text_ext, build_extractive
+from models.data_loader import load_one_text
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+    
+def preprocess_input_for_extractive(paragraph):
+    sentences = paragraph.split(". ")
+    transformed_text = ". [CLS] [SEP] ".join(sentences)
+    return transformed_text
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-task", default='ext', type=str, choices=['ext', 'abs'])
+    parser.add_argument("-encoder", default='bert', type=str, choices=['bert', 'baseline'])
+    parser.add_argument("-mode", default='train', type=str, choices=['train', 'validate', 'test', 'test_text'])
+    parser.add_argument("-bert_data_path", default='../bert_data_new/cnndm')
+    parser.add_argument("-model_path", default='../models/')
+    parser.add_argument("-result_path", default='../results/cnndm')
+    parser.add_argument("-temp_dir", default='../temp')
+    parser.add_argument("-text_src", default='')
+    parser.add_argument("-text_tgt", default='')
+
+    parser.add_argument("-batch_size", default=140, type=int)
+    parser.add_argument("-test_batch_size", default=200, type=int)
+    parser.add_argument("-max_ndocs_in_batch", default=6, type=int)
+
+    parser.add_argument("-max_pos", default=512, type=int)
+    parser.add_argument("-use_interval", type=str2bool, nargs='?',const=True,default=True)
+    parser.add_argument("-large", type=str2bool, nargs='?',const=True,default=False)
+    parser.add_argument("-load_from_extractive", default='', type=str)
+
+    parser.add_argument("-sep_optim", type=str2bool, nargs='?',const=True,default=False)
+    parser.add_argument("-lr_bert", default=2e-3, type=float)
+    parser.add_argument("-lr_dec", default=2e-3, type=float)
+    parser.add_argument("-use_bert_emb", type=str2bool, nargs='?',const=True,default=False)
+
+    parser.add_argument("-share_emb", type=str2bool, nargs='?', const=True, default=False)
+    parser.add_argument("-finetune_bert", type=str2bool, nargs='?', const=True, default=True)
+    parser.add_argument("-dec_dropout", default=0.2, type=float)
+    parser.add_argument("-dec_layers", default=6, type=int)
+    parser.add_argument("-dec_hidden_size", default=768, type=int)
+    parser.add_argument("-dec_heads", default=8, type=int)
+    parser.add_argument("-dec_ff_size", default=2048, type=int)
+    parser.add_argument("-enc_hidden_size", default=512, type=int)
+    parser.add_argument("-enc_ff_size", default=512, type=int)
+    parser.add_argument("-enc_dropout", default=0.2, type=float)
+    parser.add_argument("-enc_layers", default=6, type=int)
+
+    # params for EXT
+    parser.add_argument("-ext_dropout", default=0.2, type=float)
+    parser.add_argument("-ext_layers", default=2, type=int)
+    parser.add_argument("-ext_hidden_size", default=768, type=int)
+    parser.add_argument("-ext_heads", default=8, type=int)
+    parser.add_argument("-ext_ff_size", default=2048, type=int)
+
+    parser.add_argument("-label_smoothing", default=0.1, type=float)
+    parser.add_argument("-generator_shard_size", default=32, type=int)
+    parser.add_argument("-alpha",  default=0.6, type=float)
+    parser.add_argument("-beam_size", default=5, type=int)
+    parser.add_argument("-min_length", default=15, type=int)
+    parser.add_argument("-max_length", default=150, type=int)
+    parser.add_argument("-max_tgt_len", default=140, type=int)
+
+
+
+    parser.add_argument("-param_init", default=0, type=float)
+    parser.add_argument("-param_init_glorot", type=str2bool, nargs='?',const=True,default=True)
+    parser.add_argument("-optim", default='adam', type=str)
+    parser.add_argument("-lr", default=1, type=float)
+    parser.add_argument("-beta1", default= 0.9, type=float)
+    parser.add_argument("-beta2", default=0.999, type=float)
+    parser.add_argument("-warmup_steps", default=8000, type=int)
+    parser.add_argument("-warmup_steps_bert", default=8000, type=int)
+    parser.add_argument("-warmup_steps_dec", default=8000, type=int)
+    parser.add_argument("-max_grad_norm", default=0, type=float)
+
+    parser.add_argument("-save_checkpoint_steps", default=5, type=int)
+    parser.add_argument("-accum_count", default=1, type=int)
+    parser.add_argument("-report_every", default=1, type=int)
+    parser.add_argument("-train_steps", default=1000, type=int)
+    parser.add_argument("-recall_eval", type=str2bool, nargs='?',const=True,default=False)
+
+
+    parser.add_argument('-visible_gpus', default='-1', type=str)
+    parser.add_argument('-gpu_ranks', default='0', type=str)
+    parser.add_argument('-log_file', default='../logs/cnndm.log')
+    parser.add_argument('-seed', default=666, type=int)
+
+    parser.add_argument("-test_all", type=str2bool, nargs='?',const=True,default=False)
+    parser.add_argument("-test_from", default='')
+    parser.add_argument("-test_start_from", default=-1, type=int)
+
+    parser.add_argument("-train_from", default='')
+    parser.add_argument("-report_rouge", type=str2bool, nargs='?',const=True,default=True)
+    parser.add_argument("-block_trigram", type=str2bool, nargs='?', const=True, default=True)
+
+    args = parser.parse_args()
+    args.gpu_ranks = [int(i) for i in range(len(args.visible_gpus.split(',')))]
+    args.world_size = len(args.gpu_ranks)
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.visible_gpus
+
+    init_logger(args.log_file)
+    device = "cpu" if args.visible_gpus == '-1' else "cuda"
+    device_id = 0 if device == "cuda" else -1
+    sentence = """this Terry Jones had a love of the absurd that contributed much to the anarchic humour of Monty Python's Flying Circus. His style of visual comedy, leavened with a touch of the surreal, inspired many comedians who followed him. It was on Python that he honed his directing skills, notably on Life of Brian and The Meaning of Life. A keen historian, he wrote a number of books and fronted TV documentaries on ancient and medieval history. Terence Graham Parry Jones was born in Colwyn Bay in north Wales on 1 February 1942. His grandparents ran the local amateur operatic society and staged Gilbert and Sullivan concerts on the town's pier each year His family moved to Surrey when he was four but he always felt nostalgic about his native land. "I couldn't bear it and for the longest time I wanted Wales back," he once said. "I still feel very Welsh and feel it's where I should be really." After leaving the Royal Grammar School in Guildford, where he captained the school, he went on to read English at St Edmund Hall, Oxford. However, as he put it, he "strayed into history", the subject in which he graduated. While at Oxford he wrote sketches for the Oxford Revue and performed alongside a fellow student, Michael Palin."""
+                  
+    # sentence = preprocess_input_for_extractive(sentence)
+    gen = load_one_text(args, sentence, device)
+
+    # extractive = build_extractive(args)
+    # print(summarize_by_extractive(extractive, gen))
+
+    abstractive = build_abstractive(args)
+    print(summarize_by_abstractive(abstractive, gen))
+
